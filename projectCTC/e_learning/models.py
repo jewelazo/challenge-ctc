a454f9f6758c989769e9ba53c36f9a7e13ca6d83
jewelazo <gpalominoe@uni.pe>
2022-04-08 14:27:02 -0500

    create model Padre

--- projectCTC/e_learning/models.py
+++ projectCTC/e_learning/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Padre(models.Model):
+    name=models.CharField(max_length=30)
+    apellidos=models.CharField(max_length=60)
+    nacionalidad=models.CharField(max_length=60,blank=True,null=True)
+    telefono=models.CharField(max_length=12)
+    creado=models.DateTimeField(auto_now_add=True)
+    editado=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} {}".format(self.name,self.apellidos)
