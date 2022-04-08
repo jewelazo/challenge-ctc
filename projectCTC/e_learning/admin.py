da775a6142d93c39f72e6a99a3ce2bc5f039daeb
jewelazo <gpalominoe@uni.pe>
2022-04-08 18:10:57 -0500

    fix models Padre and Estudiante

--- projectCTC/e_learning/admin.py
+++ projectCTC/e_learning/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from e_learning.models import Padre,Estudiante,Curso,Matricula
 # Register your models here.
+admin.site.register(Padre)
+admin.site.register(Estudiante)
+admin.site.register(Curso)
+admin.site.register(Matricula)
--- projectCTC/e_learning/models.py
+++ projectCTC/e_learning/models.py
@@ -1,12 +1,20 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator,MaxValueValidator
+
+SEX_OPTIONS = (
+        ('F', 'Femenino'),
+        ('M', 'Masculino'),
+        ('T', 'They')
+)
 
 # Create your models here.
 class Padre(models.Model):
     name=models.CharField(max_length=30)
     apellidos=models.CharField(max_length=100)
     nacionalidad=models.CharField(max_length=60,blank=True,null=True)
-    edad=models.IntegerField(max_length=2,validators=[MinValueValidator(18)])
+    genero=models.CharField(max_length=1, choices=SEX_OPTIONS)
+    edad=models.IntegerField(validators=[MinValueValidator(18),MaxValueValidator(100)])
+    email=models.EmailField(max_length=70,blank=True,null=True,unique=True)
     telefono=models.CharField(max_length=12)
     creado=models.DateTimeField(auto_now_add=True)
     editado=models.DateTimeField(auto_now=True)
@@ -18,7 +26,8 @@ class Estudiante(models.Model):
     name=models.CharField(max_length=30)
     apellidos=models.CharField(max_length=100)
     nacionalidad=models.CharField(max_length=60,blank=True,null=True)
-    edad=models.IntegerField(max_length=2,validators=[MinValueValidator(5)])
+    genero=models.CharField(max_length=1, choices=SEX_OPTIONS)
+    edad=models.IntegerField(validators=[MinValueValidator(5),MaxValueValidator(17)])
     padre=models.ForeignKey(Padre, on_delete=models.CASCADE)
     creado=models.DateTimeField(auto_now_add=True)
     editado=models.DateTimeField(auto_now=True)
@@ -27,7 +36,7 @@ class Estudiante(models.Model):
         return "{} {}".format(self.name,self.apellidos)
 
 class Curso(models.Model):
-    name=models.CharField(max_length=100)
+    name=models.CharField(max_length=100,unique=True)
     precio=models.DecimalField(max_digits=5,decimal_places=2)
     creado=models.DateTimeField(auto_now_add=True)
     editado=models.DateTimeField(auto_now=True)
@@ -36,8 +45,7 @@ class Curso(models.Model):
 
 
 class Matricula(models.Model):
-    curso=models.ForeignKey(Curso)
-    estudiante=models.ForeignKey(Estudiante)
+    curso=models.ForeignKey(Curso,on_delete=models.PROTECT)
+    estudiante=models.ForeignKey(Estudiante, on_delete=models.PROTECT)
     creado=models.DateTimeField(auto_now_add=True)
     editado=models.DateTimeField(auto_now=True)
-
