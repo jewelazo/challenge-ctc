6ba9b1a26dbed977c2ee9def0c26d6384100b5de
jewelazo <gpalominoe@uni.pe>
2022-04-09 23:36:10 -0500

    create apirest Model Estudiante and fix apirest Model Padre because exists errors with swagger

--- projectCTC/e_learning/api/api.py
+++ projectCTC/e_learning/api/api.py
@@ -7,62 +7,57 @@ from e_learning.models import Padre,Estudiante
 from e_learning.api.serializers import PadreSerializer,EstudianteSerializer
 
 
-@api_view(['GET','POST'])
-def padre_api_view(request):
-    
-    # list
-    if request.method=="GET":
+class PadreAPIView(GenericAPIView):
+
+    serializer_class=PadreSerializer
+    queryset = ''
+    def get(self,request):
         padres=Padre.objects.all()
         padres_serializer=PadreSerializer(padres,many=True)
-
         return Response(padres_serializer.data,status=status.HTTP_200_OK)
-
-    # create
-    elif request.method=="POST":
+    
+    def post(self,request):
         padre_serializer=PadreSerializer(data=request.data)
         if padre_serializer.is_valid():
             padre_serializer.save()
             return Response(padre_serializer.data,status=status.HTTP_201_CREATED)
         return Response(padre_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+class PadreDetailAPIView(GenericAPIView):
 
-@api_view(["GET","PUT","DELETE"])
-def padre_detail_api_view(request,pk=None):
-    # queryset
-    padre=Padre.objects.filter(id=pk).first()
+    serializer_class=PadreSerializer
 
-    #validation
-    if padre:
 
-        # retrieve
-        if request.method=="GET":
+    def get(self,request,pk=None):
+        padre=Padre.objects.filter(pk=pk).first()
+        if padre:
             padre_serializer=PadreSerializer(padre)
             return Response(padre_serializer.data,status=status.HTTP_200_OK)
-            
-        # update
-        elif request.method=="PUT":
-            padre=Padre.objects.filter(id=pk).first()
+        return Response({"message":"No existe ese id"},status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request,pk=None):
+        padre=Padre.objects.filter(pk=pk).first()
+        if padre:
             padre_serializer=PadreSerializer(padre,data=request.data)
             if padre_serializer.is_valid():
                 padre_serializer.save()
                 return Response(padre_serializer.data,status=status.HTTP_200_OK)
             return Response(padre_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-        # delete
-        elif request.method=="DELETE":
-            padre=Padre.objects.filter(id=pk).first()
+        return Response({"message":"No existe ese id"},status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk=None):
+        padre=Padre.objects.filter(pk=pk).first()
+        if padre:
             padre.delete()
             return Response({"message":"eliminado exitosamente"},status=status.HTTP_200_OK)
-
-
-    return Response({"message":"No existe ese id"},status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":"No existe ese id"},status=status.HTTP_400_BAD_REQUEST)
 
         
 
 class EstudianteAPIView(GenericAPIView):
 
     serializer_class=EstudianteSerializer
-
+    queryset = ''
     def get(self,request):
         estudiantes=Estudiante.objects.all()
         estudiantes_serializer=EstudianteSerializer(estudiantes,many=True)
@@ -105,3 +100,54 @@ class EstudianteDetailAPIView(GenericAPIView):
         return Response({"message":"No existe ese id"},status=status.HTTP_400_BAD_REQUEST)
 
 
+
+
+# @api_view(['GET','POST'])
+# def padre_api_view(request):
+    
+#     # list
+#     if request.method=="GET":
+#         padres=Padre.objects.all()
+#         padres_serializer=PadreSerializer(padres,many=True)
+
+#         return Response(padres_serializer.data,status=status.HTTP_200_OK)
+
+#     # create
+#     elif request.method=="POST":
+#         padre_serializer=PadreSerializer(data=request.data)
+#         if padre_serializer.is_valid():
+#             padre_serializer.save()
+#             return Response(padre_serializer.data,status=status.HTTP_201_CREATED)
+#         return Response(padre_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(["GET","PUT","DELETE"])
+# def padre_detail_api_view(request,pk=None):
+#     # queryset
+#     padre=Padre.objects.filter(id=pk).first()
+
+#     #validation
+#     if padre:
+
+#         # retrieve
+#         if request.method=="GET":
+#             padre_serializer=PadreSerializer(padre)
+#             return Response(padre_serializer.data,status=status.HTTP_200_OK)
+            
+#         # update
+#         elif request.method=="PUT":
+#             padre=Padre.objects.filter(id=pk).first()
+#             padre_serializer=PadreSerializer(padre,data=request.data)
+#             if padre_serializer.is_valid():
+#                 padre_serializer.save()
+#                 return Response(padre_serializer.data,status=status.HTTP_200_OK)
+#             return Response(padre_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+#         # delete
+#         elif request.method=="DELETE":
+#             padre=Padre.objects.filter(id=pk).first()
+#             padre.delete()
+#             return Response({"message":"eliminado exitosamente"},status=status.HTTP_200_OK)
+
+
+#     return Response({"message":"No existe ese id"},status=status.HTTP_400_BAD_REQUEST)
